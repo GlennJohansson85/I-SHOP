ff736efc2fede4907afa817aa2c83a70880675f1
GlennJo <g.j@live.se>
2025-02-07 10:16:43 +0100

    d

--- static/js/toggler.js
+++ static/js/toggler.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const toggler = document.querySelector('.navbar-toggler');
+    const toggler = document.querySelector('.toggler');
 
-    toggler.addEventListener('click', function () {
-        this.classList.toggle('open');
-    });
+    function checkScreenWidth() {
+        if (window.innerWidth < 992) { // Adjust to your desired breakpoint
+            toggler.classList.add('show');
+        } else {
+            toggler.classList.remove('show');
+        }
+    }
+
+    window.addEventListener('resize', checkScreenWidth);
+    checkScreenWidth(); // Call on load to check initial width
 });
